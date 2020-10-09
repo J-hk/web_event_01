a4738f95000b08dbf7f67672c4a181844e507ede
xue <1484131202@qq.com>
2020-10-09 14:53:35 +0800

    个人中心功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,25 @@ var baseURL = "http://ajax.frontend.itheima.net";
 //    处理参数
 $.ajaxPrefilter(function(option) {
     option.url = baseURL + option.url;
+
+
+    // 对需要权限的接口配置头信息   
+    // 必须以my开头才行
+    if (option.url.indexOf("/my/") !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 3.拦截所有响应，判断身份认证信息
+    option.complete = function(res) {
+        // console.log(res);
+        // 判断，如果是身份认证失败，跳转回登录页面
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.删除本地token
+            localStorage.removeItem("token");
+            // 2.页面跳转
+            location.href = '/login.html';
+        }
+    }
 })
